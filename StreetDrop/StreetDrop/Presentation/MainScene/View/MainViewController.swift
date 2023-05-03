ac436629d0afe935d58ddfefabb04ddb40fe180f
차요셉 <jscha@raoncorp.com>
2023-05-03 22:43:35 +0900

    🛠#12: info.plist에 네이버 맵 SDK 클라이언트ID 설정 및 테스트를 위한 맵화면 출력
    - 네이버 맵 SDK 콘솔(https://console.ncloud.com/naver-service/application)에서 발급한 클라이언트ID 제대로 설정 되었는지 테스트 하기 위해, MainViewController에서 맵화면 출력

--- StreetDrop/StreetDrop/Presentation/MainScene/View/MainViewController.swift
+++ StreetDrop/StreetDrop/Presentation/MainScene/View/MainViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+import NMapsMap
+
 final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // FIXME: 코드베이스로 ViewController 잘 나오나 테스트하기 위한 빨간 바탕색 (추후 삭제 바람)
-        self.view.backgroundColor = .red
+        
+        let mapView = NMFMapView(frame: view.frame)
+        view.addSubview(mapView)
     }
 }
