0bf0288c967598324db80faf301a78e39694f7e3
차요셉 <jscha@raoncorp.com>
2023-05-03 13:57:41 +0900

    🛠#11: Storyboard 없이 코드베이스로 UI 구현하도록 세팅
    - info.plist에 스토리보드파일명 제거
    - BuildSettings에서 스토리 보드 경로 삭제
    - SceneDelegate에서 window의 루트 뷰컨트롤러로 MainViewController 지정 및 첫번째 화면으로 뜨도록 설정

--- StreetDrop/StreetDrop/Application/SceneDelegate.swift
+++ StreetDrop/StreetDrop/Application/SceneDelegate.swift
@@ -12,6 +12,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
     }
 }
