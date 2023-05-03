1d3864aa519fd2cfd23334b8876e41b4c1917d02
차요셉 <jscha@raoncorp.com>
2023-05-03 13:42:42 +0900

    🛠#11: MVVM + Repository 아키텍처에 맞게 폴더링
    - 파일 이름 앞글자가 Test인 경우, 깃 형상에 폴더가 인식되기 위함 (작업할때 지우면됨)

--- StreetDrop/StreetDrop/Presentation/MainScene/View/MainViewController.swift
+++ StreetDrop/StreetDrop/Presentation/MainScene/View/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  StreetDrop
 //
 //  Created by Joseph Cha on 2023/05/02.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
