9dba75f8fb823d6724e26be5fdc97bc4800d643a
joongkyu-park <jk.park.tech@gmail.com>
2023-05-06 02:05:17 +0900

    ✨#14: ResponseDTO 파일 추가
    - Network 레이어에서 받아온 Data 타입을 Object로 변환시켜주기 위한 타입
    - 차후 toDomain 함수를 통해 도메인 엔티티로 변경
    - 실제 OpenWeather API를 사용할 수 있는 파일 내용들을 적었다가 너무 많아서 빈 내용으로 변경

---
+++ StreetDrop/StreetDrop/Data/DataMapping/ResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  ResponseDTO.swift
+//  StreetDrop
+//
+//  Created by JoongkyuPark on 2023/05/05.
+//
+
+import Foundation
+
+// MARK: - Data Transfer Object
+
+struct ResponseDTO: Decodable {}
+
+// MARK: - Mappings to Domain
+
+extension ResponseDTO {
+    func toDomain() {}
+}
