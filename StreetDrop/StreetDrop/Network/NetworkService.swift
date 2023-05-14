bd4828ec35f44ec83991e628ea89fbbd6829cd5f
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-05-14 19:36:49 +0900

    ✨#21: NetworkService에 케이스 searchMusic 추가

--- StreetDrop/StreetDrop/Network/NetworkService.swift
+++ StreetDrop/StreetDrop/Network/NetworkService.swift
@@ -11,24 +11,35 @@ import Moya
 
 enum NetworkService {
     case getWeather(lat: String, lon: String)
+    case searchMusic(keyword: String)
 }
 
 extension NetworkService: TargetType {
-    var baseURL: URL { return URL(string: "https://api.openweathermap.org")! }
+    var baseURL: URL {
+        switch self {
+        case .getWeather:
+            return URL(string: "https://api.openweathermap.org")!
+        case .searchMusic:
+            return URL(string: "search.street-drop.com")!
+        }
+    }
+
     var path: String {
         switch self {
         case .getWeather:
             return "/data/2.5/forecast"
+        default:
+            return ""
         }
     }
-    
+
     var method: Moya.Method {
         switch self {
-        case .getWeather:
+        case .getWeather, .searchMusic:
             return .get
         }
     }
-    
+
     var task: Moya.Task {
         switch self {
         case .getWeather(let lat, let lon):
@@ -37,6 +48,10 @@ extension NetworkService: TargetType {
                                                    "units": "metric",
                                                    "appid": "a50685674910fa58b68f60c8d0d7835a"],
                                       encoding: URLEncoding.queryString)
+        case .searchMusic(let keyword):
+            return .requestParameters(
+                parameters: ["keyword": keyword],
+                encoding: URLEncoding.queryString)
         }
     }
     
@@ -48,6 +63,21 @@ extension NetworkService: TargetType {
         switch self {
         case .getWeather:
             return Data("weatherSampleData".utf8)
+        case .searchMusic:
+            return Data("""
+                        {
+                            "data": [
+                                {
+                                    "albumName": "Dynamite (DayTime Version) - EP",
+                                    "artistName": "방탄소년단",
+                                    "songName": "Dynamite",
+                                    "durationTime": "3:20",
+                                    "albumImg": "https://is2-ssl.mzstatic.com/image.../.jpg",
+                                    "albumThumbnailImg": "https://is2-ssl.mzstatic.com/.../.jpg"
+                                }
+                            ]
+                        }
+                        """.utf8)
         }
     }
 }
