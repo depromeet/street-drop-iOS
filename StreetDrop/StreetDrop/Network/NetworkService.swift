f47933f0e3f667d85dfd0001e0972868bfddffff
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-05-14 17:50:54 +0900

    ✨#19: NetworkService에 케이스 fetchPOI 추가

--- StreetDrop/StreetDrop/Network/NetworkService.swift
+++ StreetDrop/StreetDrop/Network/NetworkService.swift
@@ -11,6 +11,7 @@ import Moya
 
 enum NetworkService {
     case getWeather(lat: String, lon: String)
+    case fetchPOI(latitude: Double, longitude: Double, zoomLevel: Int)
 }
 
 extension NetworkService: TargetType {
@@ -19,12 +20,16 @@ extension NetworkService: TargetType {
         switch self {
         case .getWeather:
             return "/data/2.5/forecast"
+        case .fetchPOI:
+            return "Sample"
         }
     }
     
     var method: Moya.Method {
         switch self {
         case .getWeather:
+            return .get
+        case .fetchPOI:
             return .get
         }
     }
@@ -37,17 +42,43 @@ extension NetworkService: TargetType {
                                                    "units": "metric",
                                                    "appid": "a50685674910fa58b68f60c8d0d7835a"],
                                       encoding: URLEncoding.queryString)
+        case .fetchPOI:
+            return .requestPlain
         }
     }
     
     var headers: [String: String]? {
-        return ["Content-type": "application/json"]
+        switch self {
+        case .getWeather:
+            return ["Content-type": "application/json"]
+        default:
+            return nil
+        }
     }
 
     var sampleData: Data {
         switch self {
         case .getWeather:
             return Data("weatherSampleData".utf8)
+        case .fetchPOI:
+            return Data("""
+                    {
+                      "poi": [
+                        {
+                          "itemId": 1,
+                          "albumCover" : "http://img.com",
+                          "latitude": 89.33,
+                          "longitude": 123.222
+                        },
+                        {
+                          "itemId": 2,
+                          "albumCover" : "http://img.com",
+                          "latitude": 88.214,
+                          "longitude": 122.908
+                        }
+                      ]
+                    }
+                    """.utf8)
         }
     }
 }
