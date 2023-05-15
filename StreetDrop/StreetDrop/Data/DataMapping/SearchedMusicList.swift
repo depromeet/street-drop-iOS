5d0642f46c56298ff2991bf9d718d79cb77ca550
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-05-15 15:09:15 +0900

    ♻️#21: SearchedMusicList에서 삭제가능한 이니셜라이저 삭제

--- StreetDrop/StreetDrop/Data/DataMapping/SearchedMusicList.swift
+++ StreetDrop/StreetDrop/Data/DataMapping/SearchedMusicList.swift
@@ -23,24 +23,9 @@ struct SearchedMusicList: Codable {
             case albumImage = "albumImg"
             case albumThumbnailImage = "albumThumbnailImg"
         }
-
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            self.albumName = try container.decode(String.self, forKey: .albumName)
-            self.artistName = try container.decode(String.self, forKey: .artistName)
-            self.songName = try container.decode(String.self, forKey: .songName)
-            self.durationTime = try container.decode(String.self, forKey: .durationTime)
-            self.albumImage = try container.decode(URL.self, forKey: .albumImage)
-            self.albumThumbnailImage = try container.decode(URL.self, forKey: .albumThumbnailImage)
-        }
     }
 
     private enum CodingKeys: String, CodingKey {
         case list = "data"
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.list = try container.decode([SearchedMusicList.Music].self, forKey: .list)
     }
 }
