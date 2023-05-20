beb8192f6a027f585e1bf83e4954e0864fca867f
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-05-21 00:20:12 +0900

    💄#33: UIView에 그라데이션 적용하고 원형으로 만드는 메서드 구현
    - UIView Extension에 makeGradientCircleView()

---
+++ StreetDrop/StreetDrop/Presentation/Utils/Extension/UIView+generateCircleGradientView.swift
@@ -0,0 +1,30 @@
+//
+//  UIView+generateCircleGradientView.swift
+//  StreetDrop
+//
+//  Created by 맹선아 on 2023/05/20.
+//
+
+import UIKit
+
+extension UIView {
+    func makeGradientCircleView(
+        colors: [Any],
+        gradientLocations: [NSNumber],
+        viewBackgroundColor: UIColor
+    )
+    {
+        let axialGradient = CAGradientLayer()
+        axialGradient.type = .axial
+        axialGradient.colors = colors
+        axialGradient.locations = gradientLocations
+        axialGradient.startPoint = CGPoint(x: 0.5, y: 0)
+        axialGradient.endPoint = CGPoint(x: 0.5, y: 1)
+        axialGradient.frame = self.bounds
+
+        self.backgroundColor = viewBackgroundColor
+        self.layer.addSublayer(axialGradient)
+        self.clipsToBounds = true
+        self.layer.cornerRadius = self.frame.width/2
+    }
+}
