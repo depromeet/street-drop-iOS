94702e1661e6d65997a9e0a7e572991963719341
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-05-21 00:13:52 +0900

    💄#33: MusicDropView 뷰 요소 생성
    - ConvenienceInit 사용

--- StreetDrop/StreetDrop/Presentation/MusicDropView/View/MusicDropViewController.swift
+++ StreetDrop/StreetDrop/Presentation/MusicDropView/View/MusicDropViewController.swift
@@ -23,6 +23,8 @@ final class MusicDropViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+final class MusicDropViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
@@ -51,6 +53,12 @@ final class MusicDropViewController: UIViewController {
 
     //MARK: - 뷰 아이템 요소
     private let locationLabel: UILabel = UILabel(
+        
+    }
+
+    //MARK: - 뷰 아이템 요소
+    private let locationLabel: UILabel = UILabel(
+        text: "'성동구 성수1가 1동' 위치에 \n 음악을 드랍할게요",
         textAlignment: .center,
         font: .title2,
         numberOfLines: 2
@@ -66,6 +74,13 @@ final class MusicDropViewController: UIViewController {
     )
 
     private let artistLabel: UILabel = UILabel(
+    private let albumNameLabel: UILabel = UILabel(
+        text: "Can't Control Myself",
+        font: .body
+    )
+
+    private let singerNameLabel: UILabel = UILabel(
+        text: "태연",
         font: .caption1
     )
 
@@ -75,12 +90,14 @@ final class MusicDropViewController: UIViewController {
     )
 
     private let commentTextView: UITextView = UITextView(
+        text: "음악에 대해 하고싶은 말이 있나요?",
         backgroundColor: .darkGray,
         cornerRadius: 10,
         inset: 10
     )
 
     private let CommentGuidanceLabel: UILabel = UILabel(
+        text: "• 텍스트는 생략이 가능하며 욕설, 성희롱, 비방과 같은 내용은 삭제합니다",
         font: .caption2
     )
 
@@ -271,4 +288,14 @@ extension MusicDropViewController {
             )
         }
     }
+    
+    private let dropButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("드랍하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .darkGray
+
+        return button
+    }()
 }
