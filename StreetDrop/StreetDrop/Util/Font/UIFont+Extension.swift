2bfbc1639d8e6447aaf89d6f18650b7ccf73ea8e
JosephCha <freelove54@naver.com>
2023-05-22 23:43:57 +0900

    ✨#36: Pretendard 폰트 추가 및 적용 함수 구현

---
+++ StreetDrop/StreetDrop/Util/Font/UIFont+Extension.swift
@@ -0,0 +1,55 @@
+//
+//  UIFont+Extension.swift
+//  StreetDrop
+//
+//  Created by 차요셉 on 2023/05/22.
+//
+
+import UIKit.UIFont
+
+extension UIFont {
+    static func pretendard(
+        size: CGFloat,
+        weight: Int
+    ) -> UIFont {
+        return UIFont(
+            name: "Pretendard-\(PretendardFontWeight(rawValue: weight)?.style ?? "Regular")",
+            size: size
+        ) ?? UIFont.boldSystemFont(ofSize: 30)
+    }
+}
+
+private enum PretendardFontWeight: Int {
+    case thin = 100
+    case extraLight = 200
+    case light = 300
+    case regular = 400
+    case medium = 500
+    case semiBold = 600
+    case bold = 700
+    case extraBold = 800
+    case black = 900
+    
+    var style: String {
+        switch self {
+        case .thin:
+            return "Thin"
+        case .extraLight:
+            return "ExtraLight"
+        case .light:
+            return "Light"
+        case .regular:
+            return "Regular"
+        case .medium:
+            return "Medium"
+        case .semiBold:
+            return "SemiBold"
+        case .bold:
+            return "Bold"
+        case .extraBold:
+            return "ExtraBold"
+        case .black:
+            return "Black"
+        }
+    }
+}
