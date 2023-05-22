1150e24a6fb51db773e7ff0a0346f52a6d803cdd
JosephCha <freelove54@naver.com>
2023-05-23 00:23:52 +0900

    ✨#34: 음악 검색 뷰 구성 및 뷰모델과의 데이터 바인딩을 통해 검색 기능 추가 (Presentation 레이어)
    - 음악 검색 뷰(SearchingMusicViewController, SearchingMusicTableViewCell) 구성
    - 뷰와 뷰모델(SearchingMusicViewModel)과의 데이터 바인딩을 통해 검색 기능 구현

---
+++ StreetDrop/StreetDrop/Presentation/SearchingMusicScene/View/SearchingMusicTableViewCell.swift
@@ -0,0 +1,119 @@
+//
+//  SearchingMusicTableViewCell.swift
+//  StreetDrop
+//
+//  Created by 차요셉 on 2023/05/21.
+//
+
+import UIKit
+
+import SnapKit
+
+class SearchingMusicTableViewCell: UITableViewCell {
+    static let identifier = "SearchingMusicTableViewCell"
+    
+    private lazy var albumImage: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.layer.cornerRadius = 8.0
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var songNameLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .pretendard(size: 16, weight: 600)
+        label.setLineHeight(lineHeight: 24)
+        label.textColor = UIColor.white
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private lazy var artistNameLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .pretendard(size: 12, weight: 600)
+        label.setLineHeight(lineHeight: 18)
+        label.numberOfLines = 1
+        label.textColor = UIColor(red: 0.821, green: 0.834, blue: 0.879, alpha: 1)
+        return label
+    }()
+    
+    private lazy var durationTimeLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .pretendard(size: 13, weight: 400)
+        label.setLineHeight(lineHeight: 17)
+        label.numberOfLines = 1
+        label.textColor = UIColor(red: 0.82, green: 0.835, blue: 0.878, alpha: 1)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = .clear
+        self.selectionStyle = .none
+        self.setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been impl")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setData(music: SearchedMusicResponseDTO.Music) {
+        if let url = URL(string: music.albumImage) {
+            DispatchQueue.global().async {
+                let data = try? Data(contentsOf: url)
+                DispatchQueue.main.async {
+                    self.albumImage.image = UIImage(data: data ?? Data())
+                }
+            }
+        }
+        
+        self.songNameLabel.text = music.songName
+        self.artistNameLabel.text = music.artistName
+        self.durationTimeLabel.text = music.durationTime
+    }
+}
+
+private extension SearchingMusicTableViewCell {
+    func setUpLayout() {
+        [
+            self.albumImage,
+            self.songNameLabel,
+            self.artistNameLabel,
+            self.durationTimeLabel
+        ].forEach {
+            self.addSubview($0)
+        }
+        
+        self.albumImage.snp.makeConstraints {
+            $0.width.height.equalTo(56)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(24)
+        }
+        
+        self.songNameLabel.snp.makeConstraints {
+            $0.top.equalTo(self.albumImage.snp.top)
+            $0.leading.equalTo(self.albumImage.snp.trailing).offset(12)
+            $0.trailing.equalTo(self.durationTimeLabel.snp.leading).offset(-30)
+        }
+        
+        self.artistNameLabel.snp.makeConstraints {
+            $0.top.equalTo(self.songNameLabel.snp.bottom).offset(2)
+            $0.bottom.equalToSuperview().inset(22)
+            $0.leading.equalTo(self.albumImage.snp.trailing).offset(12)
+            $0.trailing.equalTo(self.durationTimeLabel.snp.leading).offset(-30)
+        }
+        
+        self.durationTimeLabel.snp.makeConstraints {
+            $0.width.equalTo(35)
+            $0.height.equalTo(17)
+            $0.bottom.equalTo(self.artistNameLabel.snp.bottom)
+            $0.trailing.equalToSuperview().inset(24)
+        }
+    }
+}
--- StreetDrop/StreetDrop/Presentation/SearchingMusicScene/View/SearchingMusicViewController.swift
+++ StreetDrop/StreetDrop/Presentation/SearchingMusicScene/View/SearchingMusicViewController.swift
@@ -5,4 +5,165 @@
 //  Created by 차요셉 on 2023/05/19.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+
+final class SearchingMusicViewController: UIViewController {
+    private let viewModel: DefaultSearchingMusicViewModel
+    private let disposeBag: DisposeBag = DisposeBag()
+    
+    init(viewModel: DefaultSearchingMusicViewModel = DefaultSearchingMusicViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var searchView: UIView = {
+        let uiView: UIView = UIView()
+        return uiView
+    }()
+    
+    private lazy var searchTextField: UITextField = {
+        let textField: UITextField = UITextField()
+        textField.backgroundColor = UIColor(red: 0.121, green: 0.146, blue: 0.22, alpha: 1)
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "드랍할 음악 검색",
+            attributes: [
+                .foregroundColor: UIColor(red: 0.235, green: 0.269, blue: 0.354, alpha: 1)
+            ]
+        )
+        textField.textColor = UIColor(red: 0.867, green: 0.902, blue: 0.942, alpha: 1)
+        textField.layer.cornerRadius = 8.0
+        
+        let leftPaddingView = UIView(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: 16,
+                height: 56
+            )
+        )
+        textField.leftView = leftPaddingView
+        textField.leftViewMode = .always
+        
+        textField.rightView = self.searchCancleView
+        textField.rightViewMode = .whileEditing
+        return textField
+    }()
+    
+    private lazy var searchCancleView: UIView = {
+        let uiView = UIView()
+        return uiView
+    }()
+    
+    private lazy var searchCancleButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "cancleButton"), for: .normal)
+        return button
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.setImage(UIImage(named: "backButton"), for: .normal)
+        return button
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .clear
+        tableView.register(
+            SearchingMusicTableViewCell.self,
+            forCellReuseIdentifier: SearchingMusicTableViewCell.identifier
+        )
+        tableView.rowHeight = 76
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 0.024, green: 0.046, blue: 0.12, alpha: 1)
+        bindViewModel()
+        setupLayout()
+    }
+}
+
+private extension SearchingMusicViewController {
+    func bindViewModel() {
+        let input = DefaultSearchingMusicViewModel.Input(
+            viewDidAppearEvent: .just(()),
+            searchTextFieldDidEditEvent: self.searchTextField.rx.text.orEmpty
+        )
+        let output = viewModel.convert(input: input, disposedBag: disposeBag)
+        
+        output.searchedMusicList
+            .bind(
+                to: tableView.rx.items(
+                    cellIdentifier: SearchingMusicTableViewCell.identifier,
+                    cellType: SearchingMusicTableViewCell.self
+                )
+            ) { (row, music, searchingMusicTableViewCell) in
+                searchingMusicTableViewCell.setData(music: music)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func setupLayout() {
+        [
+            self.backButton,
+            self.searchTextField
+        ].forEach {
+            self.searchView.addSubview($0)
+        }
+        
+        self.searchCancleView.addSubview(self.searchCancleButton)
+        
+        [
+            self.searchView,
+            self.tableView
+        ].forEach {
+            self.view.addSubview($0)
+        }
+        
+        self.searchView.snp.makeConstraints {
+            $0.height.equalTo(56)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        self.backButton.snp.makeConstraints {
+            $0.width.height.equalTo(32)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        self.searchTextField.snp.makeConstraints {
+            $0.height.equalTo(44)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(self.backButton.snp.trailing).offset(4)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        
+        self.searchCancleView.snp.makeConstraints {
+            $0.width.equalTo(36)
+            $0.height.equalTo(44)
+        }
+        
+        self.searchCancleButton.snp.makeConstraints {
+            $0.width.height.equalTo(20)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        
+        self.tableView.snp.makeConstraints {
+            $0.top.equalTo(self.searchView.snp.bottom).offset(26)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
