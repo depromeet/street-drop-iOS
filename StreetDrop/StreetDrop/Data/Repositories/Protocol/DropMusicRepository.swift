a40e5e00026bd59e67c55374234c7c7e1dd3f7c4
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-05-23 21:04:08 +0900

    ✨#33: DropMusic 레포지토리 Protocol 선언

---
+++ StreetDrop/StreetDrop/Data/Repositories/Protocol/DropMusicRepository.swift
@@ -0,0 +1,14 @@
+//
+//  DropMusicRepository.swift
+//  StreetDrop
+//
+//  Created by 맹선아 on 2023/05/22.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol DropMusicRepository {
+    func dropMusic(droppingInfo: DroppingInfo, adress: String, content: String) -> Single<String>
+}
