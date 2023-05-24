41c196a59f0354a2d0a4aeb49dd93df270be7906
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-05-24 16:43:22 +0900

    ✨#33: MusicDropViewModel 구현

---
+++ StreetDrop/StreetDrop/Presentation/MusicDropView/ViewModel/MusicDropViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  MusicDropViewModel.swift
+//  StreetDrop
+//
+//  Created by 맹선아 on 2023/05/21.
+//
+
+import Foundation
+
+import RxSwift
+import RxRelay
+
+final class MusicDropViewModel {
+    private let droppingInfo: DroppingInfo
+    private let locationManager: LocationManager
+    private let disposeBag: DisposeBag = DisposeBag()
+    var locationTitle: BehaviorRelay<(adress: String, text: String)>
+    var albumCover: BehaviorRelay<Data?>
+    var MusicTitle: Observable<String>
+    var artistTitle: Observable<String>
+    var commentPalceHolder: Observable<String>
+    var commentGuidanceText: Observable<String>
+    var dropButtonTitle: Observable<String>
+    var errorDescription: BehaviorRelay<String?>
+
+    init (
+        droppingInfo: DroppingInfo,
+        locationManager: LocationManager = DefaultLocationManager()
+    ) {
+        self.droppingInfo = droppingInfo
+        self.locationManager = locationManager
+        self.albumCover = BehaviorRelay(value: nil)
+        self.locationTitle = BehaviorRelay(value: (adress: "여기", text: "여기에\n음악을 드랍할게요"))
+        self.MusicTitle = Observable<String>.just(droppingInfo.music.title)
+        self.artistTitle = Observable<String>.just(droppingInfo.music.artist)
+        self.commentPalceHolder = Observable<String>.just("음악에 대해 하고싶은 말이 있나요?")
+        self.dropButtonTitle = Observable<String>.just("드랍하기")
+        self.commentGuidanceText = Observable<String>
+            .just("• 텍스트는 생략이 가능하며 욕설, 성희롱, 비방과 같은 내용은 삭제합니다")
+        self.errorDescription = BehaviorRelay(value: nil)
+    }
+
+    func fetchAdress() {
+        locationManager.fetchAdress(
+            latitude: droppingInfo.location.latitude,
+            longitude: droppingInfo.location.longitude
+        )
+        .subscribe {
+            if let adress = $0.element {
+                self.locationTitle
+                    .accept((adress: adress, text: "'\(adress)' 위치에 \n 음악을 드랍할게요"))
+            }
+        }
+        .disposed(by: disposeBag)
+    }
+
+    func fetchAlbumCover() {
+        if let albumCoverUrl = URL(string: droppingInfo.music.albumCover) {
+            DispatchQueue.global().async {
+                do {
+                    self.albumCover.accept(try Data(contentsOf: albumCoverUrl))
+                } catch {
+                    self.errorDescription.accept("albumCover 불러오기에 실패했습니다")
+                }
+            }
+        }
+    }
+
+    func drop(content: String) {
+        droppingInfo.drop(adress: locationTitle.value.adress, content: content)
+            .subscribe { response in
+                if response != "200" {
+                    self.errorDescription.accept("저장에 실패했습니다")
+                }
+            } onFailure: { error in
+                self.errorDescription.accept("저장에 실패했습니다")
+                print(error.localizedDescription)
+            }.disposed(by: disposeBag)
+    }
+}
