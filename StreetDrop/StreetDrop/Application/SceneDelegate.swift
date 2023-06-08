d946392a2aee4b398b468b7d1b2bdae5ce6ce842
JosephCha <freelove54@naver.com>
2023-06-09 00:59:01 +0900

    ✨#71: 음악 검색 화면에서, 키보드가 올라온 상태에서 최근 검색어 클릭 시, 키보드 hide 되도록 구현

--- StreetDrop/StreetDrop/Application/SceneDelegate.swift
+++ StreetDrop/StreetDrop/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = SearchingMusicViewController()
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
     }
 }
