5fba6e1c062b9adcfe51fff93fe1e27f25ae8404
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-06-10 18:38:29 +0900

    ♻️60: 드랍화면 뷰모델 인풋-아웃풋 적용, 주소(XX동)값 메인화면에서 전달받으므로 삭제

--- StreetDrop/StreetDrop/Presentation/MusicDropView/ViewModel/MusicDropViewModel.swift
+++ StreetDrop/StreetDrop/Presentation/MusicDropView/ViewModel/MusicDropViewModel.swift
@@ -1,3 +1,4 @@
+
 //
 //  MusicDropViewModel.swift
 //  StreetDrop
@@ -10,73 +11,79 @@ import Foundation
 import RxSwift
 import RxRelay
 
-final class MusicDropViewModel {
+final class MusicDropViewModel: ViewModel {
+
+    struct Input {
+        let viewDidLoadEvent: Observable<Void>
+        let tapDropButton: Observable<Void>
+        let comment: Observable<String>
+    }
+
+    struct Output {
+        var locationTitle: PublishRelay<(address: String, text: String)> = .init()
+        var musicTitle: PublishRelay<String> = .init()
+        var artistTitle: PublishRelay<String> = .init()
+        var albumImage: PublishRelay<Data> = .init()
+        let errorDescription: BehaviorRelay<String?> = .init(value: nil)
+    }
+
     private let droppingInfo: DroppingInfo
-    private let adressManager: AdressManager
+    private let musicDropModel: MusicDropModel
     private let disposeBag: DisposeBag = DisposeBag()
-    var locationTitle: BehaviorRelay<(adress: String, text: String)>
-    var dropGuideTitle: Observable<String>
-    var albumImage: BehaviorRelay<Data?>
-    var musicTitle: Observable<String>
-    var artistTitle: Observable<String>
-    var commentPlaceHolder: Observable<String>
-    var commentGuidanceText: Observable<String>
-    var dropButtonTitle: Observable<String>
-    var errorDescription: BehaviorRelay<String?>
 
     init (
         droppingInfo: DroppingInfo,
-        adressManager: AdressManager = DefaultAdressManager()
+        musicDropModel: MusicDropModel = MusicDropModel()
     ) {
         self.droppingInfo = droppingInfo
-        self.adressManager = adressManager
-        self.albumImage = BehaviorRelay(value: nil)
-        self.locationTitle = BehaviorRelay(value: (adress: "여기", text: "여기에"))
-        self.dropGuideTitle = Observable<String>.just("음악을 드랍할게요")
-        self.musicTitle = Observable<String>.just(droppingInfo.music.title)
-        self.artistTitle = Observable<String>.just(droppingInfo.music.artist)
-        self.commentPlaceHolder = Observable<String>.just("음악에 대해 하고싶은 말이 있나요?")
-        self.dropButtonTitle = Observable<String>.just("드랍하기")
-        self.commentGuidanceText = Observable<String>
-            .just("• 텍스트는 생략이 가능하며 욕설, 성희롱, 비방과 같은 내용은 삭제합니다")
-        self.errorDescription = BehaviorRelay(value: nil)
+        self.musicDropModel = musicDropModel
     }
 
-    func fetchAdress() {
-        adressManager.fetchAdress(
-            latitude: droppingInfo.location.latitude,
-            longitude: droppingInfo.location.longitude
-        )
-        .subscribe {
-            if let adress = $0.element {
-                self.locationTitle
-                    .accept((adress: adress, text: "'\(adress)' 위치에 \n 음악을 드랍할게요"))
-            }
-        }
-        .disposed(by: disposeBag)
-    }
+    func convert(input: Input, disposedBag: RxSwift.DisposeBag) -> Output {
+        let output = Output()
 
-    func fetchAlbumImage() {
-        if let albumImageUrl = URL(string: droppingInfo.music.albumImage) {
-            DispatchQueue.global().async {
-                do {
-                    self.albumImage.accept(try Data(contentsOf: albumImageUrl))
-                } catch {
-                    self.errorDescription.accept("albumImage 불러오기에 실패했습니다")
+        input.viewDidLoadEvent
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+
+                // 주소(00동)
+                let address = self.droppingInfo.location.address
+                output.locationTitle.accept((address: address, text: "'\(address)' 위치에"))
+
+                // 앨범 커버
+                if let albumImageUrl = URL(string: self.droppingInfo.music.albumImage) {
+                    DispatchQueue.global().async {
+                        do {
+                            output.albumImage.accept(try Data(contentsOf: albumImageUrl))
+                        } catch {
+                            output.errorDescription.accept("albumImage 불러오기에 실패했습니다")
+                        }
+                    }
                 }
-            }
-        }
-    }
 
-    func drop(content: String) {
-        droppingInfo.drop(adress: locationTitle.value.adress, content: content)
-            .subscribe { response in
-                if !(200...299).contains(response) {
-                    self.errorDescription.accept("저장에 실패했습니다")
-                }
-            } onFailure: { error in
-                self.errorDescription.accept("저장에 실패했습니다")
-                print(error.localizedDescription)
-            }.disposed(by: disposeBag)
+                //음악제목, 가수이름
+                output.musicTitle.accept(self.droppingInfo.music.title)
+                output.artistTitle.accept(self.droppingInfo.music.artist)
+            }).disposed(by: disposedBag)
+
+        input.tapDropButton
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+
+                var comment = ""
+                input.comment.bind { comment = $0 }.disposed(by: DisposeBag())
+
+                self.musicDropModel.drop(droppingInfo: self.droppingInfo, content: comment)
+                    .subscribe(onSuccess: { response in
+                        if !(200...299).contains(response) {
+                            output.errorDescription.accept("저장에 실패했습니다")
+                        }
+                    }, onFailure: { error in
+                        output.errorDescription.accept("저장에 실패했습니다")
+                        print(error.localizedDescription)
+                    }).disposed(by: disposedBag)
+            }).disposed(by: disposedBag)
+
+        return output
     }
 }
