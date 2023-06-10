db20e619d475fb837c882f5c2f764228997859a6
joongkyu-park <jk.park.tech@gmail.com>
2023-06-10 23:50:37 +0900

    ✨#64: Entity 생성

---
+++ StreetDrop/StreetDrop/Presentation/MainScene/Model/Entity/PoiEntity.swift
@@ -0,0 +1,17 @@
+//
+//  PoiEntity.swift
+//  StreetDrop
+//
+//  Created by JoongkyuPark on 2023/06/10.
+//
+
+import Foundation
+
+struct PoiEntity {
+    let id: Int
+    let imageURL: URL
+    let lat: Double
+    let lon: Double
+}
+
+typealias Pois = [PoiEntity]
