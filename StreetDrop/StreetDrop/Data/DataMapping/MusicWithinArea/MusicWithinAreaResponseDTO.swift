1b5b365fcff760906deadd21d1ff35f0224c9ec1
joongkyu-park <jk.park.tech@gmail.com>
2023-06-11 15:49:10 +0900

    ✨#83: MusicWithinAreaResponseDTO 수정, Request 파일 삭제

--- StreetDrop/StreetDrop/Data/DataMapping/MusicWithinArea/MusicWithinAreaResponseDTO.swift
+++ StreetDrop/StreetDrop/Data/DataMapping/MusicWithinArea/MusicWithinAreaResponseDTO.swift
@@ -7,27 +7,49 @@
 
 import Foundation
 
-// MARK: - Data Transfer Object
-
 struct MusicWithinAreaResponseDTO: Decodable {
-    let items: [ItemDTO]
-}
-
-extension MusicWithinAreaResponseDTO {
-    struct ItemDTO: Decodable {
+    let items: [Item]
+    
+    struct Item: Decodable {
         let itemID: Int
-        let music: MusicDTO
-        let content: String
+        let user: User
+        let location: String
+        let music: Music
+        let content, createdAt: String
         
         enum CodingKeys: String, CodingKey {
             case itemID = "itemId"
-            case music, content
+            case user, location, music, content, createdAt
         }
+    }
+    
+    struct Music: Decodable {
+        let title, artist: String
+        let albumImage: String
+        let genre: String
+    }
+    
+    struct User: Decodable {
+        let nickname, profileImage, musicApp: String
     }
 }
 
 extension MusicWithinAreaResponseDTO {
-    struct MusicDTO: Decodable {
-        let title, artist, albumCover: String
+    func toEntity() -> Musics {
+        return items.map {
+            .init(
+                id: $0.itemID,
+                userName: $0.user.nickname,
+                userProfileImageURL: $0.user.profileImage,
+                musicApp: $0.user.musicApp,
+                address: $0.location,
+                musicTitle: $0.music.title,
+                artist: $0.music.artist,
+                albumImageURL: $0.music.albumImage,
+                genre: $0.music.genre,
+                content: $0.content,
+                createdAt: $0.createdAt
+            )
+        }
     }
 }
