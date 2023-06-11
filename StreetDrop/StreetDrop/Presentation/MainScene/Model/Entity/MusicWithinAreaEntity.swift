d498a556b13d5cc81f8b4ef8b475ce022d77b650
joongkyu-park <jk.park.tech@gmail.com>
2023-06-11 15:50:35 +0900

    ✨#83: MusicWithinAreaEntity 추가

---
+++ StreetDrop/StreetDrop/Presentation/MainScene/Model/Entity/MusicWithinAreaEntity.swift
@@ -0,0 +1,24 @@
+//
+//  MusicWithinAreaEntity.swift
+//  StreetDrop
+//
+//  Created by JoongkyuPark on 2023/06/11.
+//
+
+import Foundation
+
+struct MusicWithinAreaEntity {
+    let id: Int
+    let userName: String
+    let userProfileImageURL: String
+    let musicApp: String
+    let address: String
+    let musicTitle: String
+    let artist: String
+    let albumImageURL: String
+    let genre: String
+    let content: String
+    let createdAt: String
+}
+
+typealias Musics = [MusicWithinAreaEntity]
