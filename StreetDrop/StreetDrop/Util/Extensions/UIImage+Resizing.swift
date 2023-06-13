2e7bea4c38c5ab2e350e41ce0f9c5c39abdae3c8
JosephCha <freelove54@naver.com>
2023-06-13 16:35:47 +0900

    ✨ 이미지 캐싱 로직, 이미지 서버 url load 로직 통합 및 적용

--- StreetDrop/StreetDrop/Util/Extensions/UIImage+Resizing.swift
+++ StreetDrop/StreetDrop/Util/Extensions/UIImage+Resizing.swift
@@ -8,27 +8,6 @@
 import UIKit
 
 extension UIImage {
-    static func load(with url: String, completion: @escaping (UIImage?) -> ()) {
-        DispatchQueue.global(qos: .background).async {
-            let cachedKey = NSString(string: url)
-            if let cachedImage = ImageCacheService.shared.object(forKey: cachedKey) {
-                completion(cachedImage)
-            }
-            
-            guard let url = URL(string: url) else { return }
-            URLSession.shared.dataTask(with: url) { (data, _, error) in
-                guard error == nil else {
-                    completion(nil)
-                    return
-                }
-                if let data = data, let image = UIImage(data: data) {
-                    ImageCacheService.shared.setObject(image, forKey: cachedKey)
-                    completion(image)
-                }
-            }.resume()
-        }
-    }
-    
     func resized(to newSize: CGSize) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
         self.draw(in: CGRect(origin: .zero, size: newSize))
---
+++ StreetDrop/StreetDrop/Util/Extensions/UIImageView+ImageCache.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+ImageCache.swift
+//  StreetDrop
+//
+//  Created by 차요셉 on 2023/06/13.
+//
+
+import UIKit
+
+import RxSwift
+
+extension UIImageView {
+    func setImage(with url: String, isUsingDiskCache: Bool = false, disposeBag: DisposeBag) {
+        ImageCacheService.shared.setImage(url, isUsingDiskCache: isUsingDiskCache)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] image in
+                self?.image = UIImage(data: image)
+            })
+            .disposed(by: disposeBag)
+    }
+}
