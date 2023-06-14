dfc3a3e8744fd33dd1b97b6ae613461ce530db3f
joongkyu-park <jk.park.tech@gmail.com>
2023-06-14 22:57:05 +0900

    ✨#81: UIImage에 cornerRadius를 줄 수 있는 Extension 추가

---
+++ StreetDrop/StreetDrop/Util/Extensions/UIImage+cornerRadius.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+cornerRadius.swift
+//  StreetDrop
+//
+//  Created by JoongkyuPark on 2023/06/14.
+//
+
+import UIKit
+
+extension UIImage {
+       public func withRoundedCorners(radius: CGFloat? = nil) -> UIImage? {
+           let maxRadius = min(size.width, size.height) / 2
+           let cornerRadius: CGFloat
+           if let radius = radius, radius > 0 && radius <= maxRadius {
+               cornerRadius = radius
+           } else {
+               cornerRadius = maxRadius
+           }
+           UIGraphicsBeginImageContextWithOptions(size, false, scale)
+           let rect = CGRect(origin: .zero, size: size)
+           UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius).addClip()
+           draw(in: rect)
+           let image = UIGraphicsGetImageFromCurrentImageContext()
+           UIGraphicsEndImageContext()
+           return image
+       }
+   }
