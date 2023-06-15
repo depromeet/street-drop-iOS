0dc0a227f2ea57f83e7d03572eb0931a50732c76
JosephCha <freelove54@naver.com>
2023-06-16 01:19:27 +0900

    💄스플래시 화면 추가 (sleep 1초)

--- StreetDrop/StreetDrop/Application/AppDelegate.swift
+++ StreetDrop/StreetDrop/Application/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        sleep(1)
         return true
     }
 
