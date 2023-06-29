bc46fde5a98f365dccbf78111cf964d449424b56
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-06-30 02:03:37 +0900

    ♻️#149: 서버와 맞추기 위해 신고 네이밍 변경(report -> claim)

--- StreetDrop/StreetDrop/Presentation/CommunityView/ViewModel/ClaimModalViewModel.swift
+++ StreetDrop/StreetDrop/Presentation/CommunityView/ViewModel/ClaimModalViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  ReportModalViewModel.swift
+//  ClaimModalViewModel.swift
 //  StreetDrop
 //
 //  Created by 맹선아 on 2023/06/27.
@@ -10,57 +10,59 @@ import Foundation
 import RxSwift
 import RxRelay
 
-final class ReportModalViewModel {
+final class ClaimModalViewModel {
     struct Input {
-        let tapReportOption: Observable<Report>
+        let tapClaimOption: Observable<Claim>
         let tapSendButton: Observable<Void>
     }
 
     struct Output {
-        var reportStatusResults: PublishRelay<String> = .init()
+        var claimStatusResults: PublishRelay<String> = .init()
     }
 
     private let itemID: Int
-    private var reportOption: Report?
+    private var claimOption: Claim?
     private let communityModel: CommunityModel
 
     init(itemID: Int,
-         reportOption: Report? = nil,
+         claimOption: Claim? = nil,
          communityModel: CommunityModel = DefaultCommunityModel()
     ) {
         self.itemID = itemID
-        self.reportOption = reportOption
+        self.claimOption = claimOption
         self.communityModel = communityModel
     }
 
     func convert(input: Input, disposedBag: DisposeBag) -> Output {
         let output = Output()
 
-        input.tapReportOption
+        input.tapClaimOption
             .subscribe(onNext: { [weak self] in
-                self?.reportOption = $0
+                self?.claimOption = $0
             }).disposed(by: disposedBag)
 
         input.tapSendButton
             .subscribe(onNext: { [weak self] in
                 guard let self = self,
-                      let reportOption = self.reportOption else { return }
+                      let claimOption = self.claimOption else { return }
 
                 self.communityModel.claimComment(
                     itemID: self.itemID,
-                    reason: reportOption.title
+                    reason: claimOption.title
                 )
                     .subscribe(onSuccess: { response in
                         if (200...299).contains(response) {
                             // ✅ TODO: 토스트 문구 변경하기
-                            output.reportStatusResults.accept("신고 성공 토스트 문구")
+                            print("\(response), 신고 성공 토스트 문구")
+                            output.claimStatusResults.accept("신고 성공 토스트 문구")
                         } else if response == 409 {
-                            output.reportStatusResults.accept("이미 신고했습니다. 토스트 문구")
+                            print("\(response), 이미 신고했습니다. 토스트 문구")
+                            output.claimStatusResults.accept("이미 신고했습니다. 토스트 문구")
                         } else {
-                            output.reportStatusResults.accept("네트워크 확인 토스트 문구")
+                            output.claimStatusResults.accept("네트워크 확인 토스트 문구")
                         }
                     }, onFailure: { error in
-                        output.reportStatusResults.accept("네트워크 확인 토스트 문구")
+                        output.claimStatusResults.accept("네트워크 확인 토스트 문구")
                         print(error.localizedDescription)
                     }).disposed(by: disposedBag)
             }).disposed(by: disposedBag)
