00759cab1648647585f885836bebd719f0f5c4d9
joongkyu-park <jk.park.tech@gmail.com>
2023-07-11 01:40:29 +0900

    ✨#158: 닉네임 변경하는 화면 개발, 마이페이지 화면과 연결

---
+++ StreetDrop/StreetDrop/Presentation/MyPage/View/NicknameEditViewController.swift
@@ -0,0 +1,159 @@
+//
+//  NicknameEditViewController.swift
+//  StreetDrop
+//
+//  Created by JoongkyuPark on 2023/07/10.
+//
+
+import UIKit
+
+final class NicknameEditViewController: UIViewController {
+
+    // MARK: - UI
+    
+    private lazy var topView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.gray900
+        return view
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "backButton")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = UIColor.gray100
+        button.imageView?.contentMode = .scaleAspectFit
+        return button
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .pretendard(size: 16, weightName: .bold)
+        label.textColor = UIColor.gray100
+        label.text = "닉네임 변경"
+        return label
+    }()
+    
+    private lazy var containerStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var nicknameTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .pretendard(size: 16, weightName: .medium)
+        textField.textColor = UIColor.gray100
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 12
+        textField.backgroundColor = UIColor.gray700
+        
+        let padding = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: textField.frame.height))
+        textField.leftView = padding
+        textField.rightView = padding
+        textField.leftViewMode = .always
+        textField.rightViewMode = .always
+        
+        return textField
+    }()
+    
+    private lazy var guideLabel: UILabel = {
+        let label = UILabel()
+        label.font = .pretendard(size: 14, weightName: .medium)
+        label.textColor = UIColor.gray300
+        label.text = "최소 1자에서 10자 이내로 입력해주세요"
+        return label
+    }()
+    
+    private lazy var confirmButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 12
+        button.backgroundColor = UIColor.gray400
+        button.setTitle("변경하기", for: .normal)
+        button.titleLabel?.font = .pretendard(size: 16, weightName: .bold)
+        button.setTitleColor(UIColor.gray300, for: .normal)
+        button.isEnabled = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+}
+
+private extension NicknameEditViewController {
+    
+    // MARK: - UI
+    
+    func configureUI() {
+        
+        // MARK: - ViewController
+        
+        self.view.backgroundColor = UIColor.gray900
+        
+        // MARK: - Top View
+        
+        self.view.addSubview(topView)
+        self.topView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(8)
+            make.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(60)
+        }
+        
+        // MARK: - Back Button
+        
+        self.topView.addSubview(backButton)
+        self.backButton.snp.makeConstraints { make in
+            make.width.height.equalTo(32)
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview()
+        }
+        
+        // MARK: - Title Label
+        
+        self.topView.addSubview(titleLabel)
+        self.titleLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        // MARK: - Container StackView
+        
+        self.view.addSubview(containerStackView)
+        self.containerStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(24)
+            make.top.equalTo(self.topView.snp.bottom).offset(8)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(8)
+        }
+        
+        // MARK: - Nickname TextField
+        
+        self.containerStackView.addArrangedSubview(nicknameTextField)
+        self.nicknameTextField.snp.makeConstraints { make in
+            make.height.equalTo(56)
+        }
+        
+        // MARK: - Guide Label
+        
+        self.containerStackView.addArrangedSubview(guideLabel)
+        
+        // MARK: - Confirm Button
+        
+        self.containerStackView.addArrangedSubview(
+            {
+                let spacerView = UIView()
+                spacerView.translatesAutoresizingMaskIntoConstraints = false
+                spacerView.backgroundColor = UIColor.clear
+                spacerView.setContentHuggingPriority(.defaultLow, for: .vertical)
+                return spacerView
+            }()
+        )
+        
+        self.containerStackView.addArrangedSubview(confirmButton)
+        self.confirmButton.snp.makeConstraints { make in
+            make.height.equalTo(56)
+            make.leading.trailing.equalToSuperview()
+        }
+    }
+}
