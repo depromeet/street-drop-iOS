1d7a877c7ffb374479e80f0944061100887143c2
JosephCha <freelove54@naver.com>
2023-07-16 01:34:16 +0900

    ✨#160: 로컬에 저장된 음악앱 가져오는 데이터 로직 및 음악앱 서비스 변경 데이터 로직 추가

---
+++ StreetDrop/StreetDrop/Data/Repositories/Protocol/Setting/SettingsRepository.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsRepository.swift
+//  StreetDrop
+//
+//  Created by 차요셉 on 2023/07/09.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol SettingsRepository {
+    func fetchMymusicAppFromLocal() -> Single<MusicApp>
+    func updateUsersMusicAppToServer(musicAppQueryString: String) -> Single<MusicApp>
+}
