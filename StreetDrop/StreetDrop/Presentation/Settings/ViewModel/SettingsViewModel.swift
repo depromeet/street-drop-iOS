c1fefc7aba6ccc173da10537544b72e4a80428e1
JosephCha <freelove54@naver.com>
2023-07-16 01:36:18 +0900

    ✨#160: 로컬에 저장된 음악앱 가져오는 비즈니스 로직 및 음악앱 서비스 변경 비즈니스 로직 추가

--- StreetDrop/StreetDrop/Presentation/Settings/ViewModel/SettingsViewModel.swift
+++ StreetDrop/StreetDrop/Presentation/Settings/ViewModel/SettingsViewModel.swift
@@ -15,14 +15,58 @@ protocol SettingsViewModel: ViewModel {
 }
 
 final class DefaultSettingsViewModel: SettingsViewModel {
-    private let disposeBag: DisposeBag = DisposeBag()
-    struct Input {}
+    private let model: SettingsModel
     
-    struct Output {}
+    init(model: SettingsModel = DefaultSettingsModel()) {
+        self.model = model
+    }
+    struct Input {
+        let viewDidLoadEvent: Observable<Void>
+        let musicAppButtonEvent: Observable<String>
+    }
+    
+    struct Output {
+        let currentMusicApp: PublishRelay<MusicApp> = .init()
+        let savedMusicAppInServer: PublishRelay<MusicApp> = .init()
+    }
     
     func convert(input: Input, disposedBag: DisposeBag) -> Output {
         let output = Output()
         
+        input.viewDidLoadEvent
+            .bind { [weak self] in
+                self?.fetchMymusicAppFromLocal(output: output, disposeBag: disposedBag)
+            }
+            .disposed(by: disposedBag)
+        
+        input.musicAppButtonEvent
+            .bind(onNext: { [weak self] musicAppQueryString in
+                self?.selectMusicApp(musicAppQueryString: musicAppQueryString, output: output, disposeBag: disposedBag)
+            })
+            .disposed(by: disposedBag)
+        
         return output
+    }
+}
+
+private extension DefaultSettingsViewModel {
+    func selectMusicApp(musicAppQueryString: String, output: Output, disposeBag: DisposeBag) {
+        self.model.updateUsersMusicAppToServer(musicAppQueryString: musicAppQueryString)
+            .subscribe { savedMusicAppInServer in
+                output.savedMusicAppInServer.accept(savedMusicAppInServer)
+            } onFailure: { error in
+                print(error.localizedDescription)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func fetchMymusicAppFromLocal(output: Output, disposeBag: DisposeBag) {
+        self.model.fetchMymusicAppFromLocal()
+            .subscribe { myMusicApp in
+                output.currentMusicApp.accept(myMusicApp)
+            } onFailure: { error in
+                print(error.localizedDescription)
+            }
+            .disposed(by: disposeBag)
     }
 }
