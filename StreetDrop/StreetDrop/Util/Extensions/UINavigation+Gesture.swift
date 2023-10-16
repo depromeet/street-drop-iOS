39b94bb1bab3ab8be622d29be7a43fe6564da167
thoonk <dkrmrm878@gmail.com>
2023-10-16 21:36:08 +0900

    ✨#151: 네비게이션 스와이프 백 제스쳐 추가

---
+++ StreetDrop/StreetDrop/Util/Extensions/UINavigation+Gesture.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigation+Gesture.swift
+//  StreetDrop
+//
+//  Created by thoonk on 2023/10/16.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
