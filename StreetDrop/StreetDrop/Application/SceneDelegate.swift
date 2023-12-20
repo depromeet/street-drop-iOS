fa78967b972635f7a860382ddd46778a48331530
thoonk <dkrmrm878@gmail.com>
2023-12-20 23:10:22 +0900

    ✨#263: 유니버셜 링크 설정

--- StreetDrop/StreetDrop/Application/SceneDelegate.swift
+++ StreetDrop/StreetDrop/Application/SceneDelegate.swift
@@ -11,11 +11,36 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
+    }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+              let incomingURL = userActivity.webpageURL,
+              let components = URLComponents(url: incomingURL, resolvingAgainstBaseURL: true)
+        else {
+            return
+        }
+        
+        let path = components.path
+        guard let params = components.queryItems else { return }
+        
+        print("path = \(path)")
+        print("params = \(params)")
+        
+        if let itemID = params.first(where: { $0.name == "itemID"})?.value {
+            print("itemID = \(itemID)")
+        } else {
+            print("itemID missing")
+        }
     }
 }
