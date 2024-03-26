a83030d9de914f3361ea6944ad010e4a30255563
thoonk <dkrmrm878@gmail.com>
2024-03-26 17:54:10 +0900

    ✨#281: 마이페이지 레벨 가이드뷰 RoundCorner 적용 및 레이아웃 조정

---
+++ StreetDrop/StreetDrop/Presentation/Utils/Extension/UIView+RoundCorners.swift
@@ -0,0 +1,25 @@
+//
+//  UIView+RoundCorners.swift
+//  StreetDrop
+//
+//  Created by thoonk on 2024/03/25.
+//
+
+import UIKit
+
+extension UIView {
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        if #available(iOS 11.0, *) {
+            clipsToBounds = true
+            layer.cornerRadius = radius
+            layer.masksToBounds = true
+            layer.maskedCorners = CACornerMask(rawValue: corners.rawValue)
+        } else {
+            let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners,
+                                    cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            layer.mask = mask
+        }
+    }
+}
