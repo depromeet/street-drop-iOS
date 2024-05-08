463437282fcf1c8f436a33abe4cf3e5f667065a7
Taehoon Kim <dkrmrm878@gmail.com>
2024-05-08 13:41:09 +0900

    ✨#281: GradientProgressBar Init 변경(Only codebase)
    Co-authored-by: Joseph Cha <freelove54@naver.com>

--- StreetDrop/StreetDrop/Presentation/MyPage/View/GradientProgressBar.swift
+++ StreetDrop/StreetDrop/Presentation/MyPage/View/GradientProgressBar.swift
@@ -37,9 +37,10 @@ final class GradientProgressBar: UIView {
         setup()
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     }
     
     override func layoutSubviews() {
