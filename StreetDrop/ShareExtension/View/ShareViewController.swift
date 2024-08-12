8935808c6fd9b33a7e9458ff9d2e17424552c97c
Joseph Cha <freelove54@naver.com>
2024-08-12 17:16:40 +0900

    🛠#297: Share Extension에서 사용할 파일들 타겟멤버쉽 추가 및 사용해야할 SDK들 의존성 추가

--- StreetDrop/ShareExtension/View/ShareViewController.swift
+++ StreetDrop/ShareExtension/View/ShareViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 import Social
 
+import RxSwift
+
 final class ShareViewController: SLComposeServiceViewController {
-    
+    private let viewModel: ShareViewModel = .init()
+    private let disposeBag: DisposeBag = .init()
     
     override func isContentValid() -> Bool {
         return true
@@ -22,5 +25,21 @@ final class ShareViewController: SLComposeServiceViewController {
     override func configurationItems() -> [Any]! {
         return []
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindViewModel()
+    }
+}
 
+private extension ShareViewController {
+    func bindViewModel() {
+        let input: ShareViewModel.Input = .init(viewDidLoadEvent: .just(Void()))
+        let output: ShareViewModel.Output = viewModel.convert(
+            input: input,
+            disposedBag: disposeBag
+        )
+    }
+    
+    
 }
